da53719a5695940d4ab5497f1dc2776e3adde80b
Joel Thorstensson <joel@oed.earth>
2021-02-06 15:57:08 +0100

    Cleanup

--- src/app.ts
+++ src/app.ts
@@ -11,16 +11,12 @@ declare global {
     did?: DID
     idx?: IDX
     ceramic?: CeramicApi
-    SECRET_NOTES_DEFINITION?: string
   }
 }
 
 interface SecretNotes {
   notes: any[]
 }
-
-const SECRET_NOTES_DEFINITION = 'kjzl6cwe1jw14b03qkg5rl0dmq44yjayvku5yvca69fhokexzodwpbjb2zgqusj'
-window.SECRET_NOTES_DEFINITION = SECRET_NOTES_DEFINITION
 
 const ceramicPromise = createCeramic()
 
@@ -40,6 +36,58 @@ const ethAddressToDID = async (address: string): Promise<string> => {
     }
   })
   return caip10Doc?.content
+}
+
+const updateProfile = async () => {
+  const name = (document.getElementById('name') as HTMLInputElement).value
+  const description = (document.getElementById('description') as HTMLInputElement).value
+  await window.idx?.set('basicProfile', { name, description })
+}
+
+const createNote = async () => {
+  const record = (await window.idx?.get('secretNotes')) as SecretNotes || { notes: [] }
+  const recipient = (document.getElementById('recipient') as HTMLInputElement).value
+  const note = (document.getElementById('note') as HTMLInputElement).value
+  const noteData = { recipient, note }
+  const recipients = [window.did?.id as string] // always make ourselves a recipient
+  if (recipient) recipients.push(recipient)
+  const encryptedNote = await window.did?.createDagJWE(noteData, recipients)
+  record.notes.push(encryptedNote)
+  await window.idx?.set('secretNotes', record)
+}
+
+const loadNotes = async () => {
+  const noteContainer = document.getElementById('allNotes')
+  // @ts-ignore
+  noteContainer?.innerHTML = ''
+  let user = (document.getElementById('user') as HTMLInputElement).value || window.did?.id
+  if (user && !user.startsWith('did')) {
+    user = await ethAddressToDID(user)
+  }
+  const record = (await window.idx?.get('secretNotes', user)) as SecretNotes
+
+  record?.notes.map(async (encryptedNote, mapindex) => {
+    try {
+      const { recipient, note } = await window.did?.decryptDagJWE(encryptedNote) as Record<string, any>
+      let noteEntry = '<p>'
+      if (recipient) {
+        noteEntry += '<b>Recipient:</b> ' + (recipient || '--') + `<span id="name${mapindex}"></span>`
+        addNameToNote(recipient, 'name' + mapindex)
+      }
+      noteEntry += '<br /><b>Note:</b> ' + note + '</p><hr />'
+      // @ts-ignore
+      noteContainer?.innerHTML += noteEntry
+    } catch (e) {}
+  })
+}
+
+const addNameToNote = async (recipient: string, elemId: string): Promise<void> => {
+  const { name } = await window.idx?.get('basicProfile', recipient) as any || {}
+  if (name) {
+    const nameContainer = document.getElementById(elemId)
+    // @ts-ignore
+    nameContainer?.innerHTML = `<br /><b>Recipient name:</b> ${name}`
+  }
 }
 
 document.getElementById('bauth')?.addEventListener('click', () => {
@@ -65,40 +113,17 @@ document.getElementById('bauth')?.addEventListener('click', () => {
 
 
 document.getElementById('updateProfile')?.addEventListener('click', async () => {
-  const name = (document.getElementById('name') as HTMLInputElement).value
-  const description = (document.getElementById('description') as HTMLInputElement).value
-  await window.idx?.set('basicProfile', { name, description })
-  console.log('Basic Profile set for', name)
+  // @ts-ignore
+  document.getElementById('profileloading')?.style?.display = 'block';
+  await updateProfile()
+  // @ts-ignore
+  document.getElementById('profileloading')?.style?.display = 'none';
 })
 
 document.getElementById('loadNotes')?.addEventListener('click', async () => {
-  const noteContainer = document.getElementById('allNotes')
-  // @ts-ignore
-  noteContainer?.innerHTML = ''
   // @ts-ignore
   document.getElementById('loadloading')?.style?.display = 'block';
-  let user = (document.getElementById('user') as HTMLInputElement).value || window.did?.id
-  if (user && !user.startsWith('did')) {
-    user = await ethAddressToDID(user)
-  }
-  const record = (await window.idx?.get(SECRET_NOTES_DEFINITION, user)) as SecretNotes
-
-  record?.notes.map(async encryptedNote => {
-    try {
-      const { recipient, note } = await window.did?.decryptDagJWE(encryptedNote) as Record<string, any>
-      let noteEntry = '<p>'
-      if (recipient) {
-        noteEntry += '<b>Recipient:</b> ' + recipient || '--'
-        const { name } = await window.idx?.get('basicProfile', recipient) as any || {}
-        if (name) {
-          noteEntry += '<br /><b>Recipient name:</b> ' + name + '<br />'
-        }
-      }
-      noteEntry += '<br /><b>Note:</b> ' + note + '</p><hr />'
-      // @ts-ignore
-      noteContainer?.innerHTML += noteEntry
-    } catch (e) {}
-  })
+  await loadNotes()
   // @ts-ignore
   document.getElementById('loadloading')?.style?.display = 'none';
 })
@@ -106,15 +131,7 @@ document.getElementById('loadNotes')?.addEventListener('click', async () => {
 document.getElementById('createNote')?.addEventListener('click', async () => {
   // @ts-ignore
   document.getElementById('createloading')?.style?.display = 'block';
-  const record = (await window.idx?.get(SECRET_NOTES_DEFINITION)) as SecretNotes || { notes: [] }
-  const recipient = (document.getElementById('recipient') as HTMLInputElement).value
-  const note = (document.getElementById('note') as HTMLInputElement).value
-  const noteData = { recipient, note }
-  const recipients = [window.did?.id as string] // always make ourselves a recipient
-  if (recipient) recipients.push(recipient)
-  const encryptedNote = await window.did?.createDagJWE(noteData, recipients)
-  record.notes.push(encryptedNote)
-  await window.idx?.set(SECRET_NOTES_DEFINITION, record)
+  await createNote()
   // @ts-ignore
   document.getElementById('createloading')?.style?.display = 'none';
 })
--- src/idx.ts
+++ src/idx.ts
@@ -7,8 +7,12 @@ declare global {
   }
 }
 
+const aliases = {
+  secretNotes: 'kjzl6cwe1jw14b03qkg5rl0dmq44yjayvku5yvca69fhokexzodwpbjb2zgqusj'
+}
+
 export function createIDX(ceramic: CeramicApi): IDX {
-  const idx = new IDX({ ceramic })
+  const idx = new IDX({ ceramic, aliases })
   window.idx = idx
   return idx
 }
